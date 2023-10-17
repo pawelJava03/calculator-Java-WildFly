cde540db6e47e59890e40e7c5388225a9435c341
Paweł <pa.st.a@wp.pl>
2023-10-17 21:04:04 +0200

    v1.1.calculator

--- src/main/java/com/example/serwlety/Calculator.java
+++ src/main/java/com/example/serwlety/Calculator.java
@@ -3,6 +3,8 @@ package com.example.serwlety;
 import java.io.IOException;
 import java.io.PrintWriter;
 
+import org.apache.commons.lang3.StringUtils;
+
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
@@ -18,47 +20,46 @@ public class Calculator extends HttpServlet {
         response.setCharacterEncoding("utf-8");
         PrintWriter out = response.getWriter();
         out.println("""
-   			 <!DOCTYPE html>
-   			 <html>
-   				 <head>
-   				 <title>Calculator</title>
-   				 <link rel="stylesheet" href="style.css">
-   			 </head>
-   			 <body>
-   					 <h1>Servlet calculator</h1>
-   	 """);
+				<!DOCTYPE html>
+				<html>
+					<head>
+						<title>Calculator</title>
+						<link rel="stylesheet" href="style.css">
+					</head>
+					<body>
+						<h1>Servlet Calculator</h1>
+		""");
 
         out.println("""
-   			 <form>
-                	<input name="num1" type="text">
-                	<select name="operation">
-                    	<option value='+'>+</option>
-                    	<option value='-'>-</option>
-                    	<option value='*'>*</option>
-                    	<option value='/'>/</option>
-                	</select>
-                	<input name="num2" type="text">
-                	<br/>
-                	<button>Execute</button>
-   			 </form>
-   			 """);
+				<form>
+	                <input name="num1" type="number">
+	                <select name="operation">
+	                    <option value='+'>+</option>
+	                    <option value='-'>-</option>
+	                    <option value='*'>*</option>
+	                    <option value='/'>/</option>
+	                </select>
+	                <input name="num2" type="number">
+	                <br/>
+	                <button>Execute</button>
+				</form>
+				""");
 
         String arg1 = request.getParameter("num1");
         String arg2 = request.getParameter("num2");
-        String mathOperation = request.getParameter("math operation");
+        String operation = request.getParameter("operation");
 
-        if(arg1 != null && arg2 != null) {
+        if(StringUtils.isNoneBlank(arg1, arg2, operation)) {
             long num1 = Long.parseLong(arg1);
             long num2 = Long.parseLong(arg2);
-            long sum = oblicz(num2, num2, mathOperation);
-            out.print("<div>Result: " + sum + "</div>");
+            long result = oblicz(num1, num2, operation);
+            out.printf("<div class='result'>%d %s %d = <strong>%d</strong></div>",
+                    num1, operation, num2, result);
         }
-
         out.println("</body></html>");
     }
 
-
-    //math operations
+    // math operations switch
     private long oblicz(long num1, long num2, String operation) {
         return switch(operation) {
             case "+" -> num1 + num2;
@@ -69,5 +70,4 @@ public class Calculator extends HttpServlet {
         };
     }
 }
-
 
